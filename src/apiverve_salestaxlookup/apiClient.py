dda18ac58f278b986eb3ed3cbb380e842388db11
APIVerve Team <hello@apiverve.com>
2025-03-28 17:48:03 -0500

    Bug Fixes

--- src/apiverve_salestaxlookup/apiClient.py
+++ src/apiverve_salestaxlookup/apiClient.py
@@ -10,10 +10,10 @@ class SalestaxAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/salestax'
+        self.base_url = 'https://api.apiverve.com/v1/salestax'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
